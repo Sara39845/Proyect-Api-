4887346930faa845014b7aca5ab08e86fb36b06d
Sara <perezsara398@gmail.com>
2024-10-23 08:38:11 -0500

    segundo commit API

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-const express = require("express")
+const express = require("express");
 const routerApi = require("./routes");
 const cors = require('cors');
 
-
-const {logerrors, errorHandler, boomErrorHandler} = require("./Middleware/error.handler");
-
+const { logerrors, errorHandler, boomErrorHandler } = require("./Middleware/error.handler");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080','https://myapp.co'];
-const options={
-    origin:(origin,callback)=>{
-    if(whitelist.includes(origin)){
-        callback(null,true);}
-    else
-    {callback(new Error('no permitido'));
-
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true); // Permite solicitudes sin origen (por ejemplo, en Postman)
+    } else {
+      callback(new Error('no permitido'));
     }
-}
-}
-app.use(cors());
+  }
+};
+
+// Aplica CORS con las opciones definidas
+app.use(cors(options));
 
 app.get("/", (req, res) => {
-    res.send("Hola server");
-})
+  res.send("Hola server");
+});
+
 app.get('/nueva-ruta', (req, res) => {
-    res.send('Hola, soy una nueva ruta');
-  });
-  
-routerApi (app);
+  res.send('Hola, soy una nueva ruta');
+});
+
+routerApi(app);
 
 app.use(logerrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log("My port" + port);
+  console.log("My port " + port);
 });
